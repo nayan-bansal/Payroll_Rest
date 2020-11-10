c1af2ae017d67abf50c22193b3115461e78eb486
nayan-bansal <nayan.7991@gmail.com>
2020-11-10 16:37:04 +0530

    Added: Multiple Emplloyees to JSON Server

--- EmployeePayrollRestMain.java
+++ EmployeePayrollRestMain.java
@@ -1,5 +1,32 @@
 package Employee.Payroll_Employee;
 
-public class EmployeePayrollRestMain {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmployeePayrollRestMain {
+	private List<Employee> empPayrollList;
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
+	}
+
+	public EmployeePayrollRestMain() {
+	}
+
+	public EmployeePayrollRestMain(List<Employee> employeeList) {
+	
+		this.empPayrollList = new ArrayList<>(employeeList); // Use new memory not the same as provided by client to
+																// avoid confusion
+
+	}
+
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.REST_IO))
+			return empPayrollList.size();
+		return 0;
+	}
+
+	public void addEmployeeToPayroll(Employee employeeData, IOService ioService) {
+		empPayrollList.add(employeeData);
+	}
 }
